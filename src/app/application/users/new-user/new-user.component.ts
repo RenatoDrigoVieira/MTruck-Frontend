6699d96079a18fd86255aa273537be9f6ba04318
Renato Vieira <renato@getconnect.com.br>
2020-11-15 16:38:50 -0300

    Melhorando validação de senha

--- src/app/application/users/new-user/new-user.component.ts
+++ src/app/application/users/new-user/new-user.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { User } from 'src/app/models/User';
@@ -7,51 +13,44 @@ import { User } from 'src/app/models/User';
 @Component({
   selector: 'app-new-user',
   templateUrl: './new-user.component.html',
-  styleUrls: ['./new-user.component.scss']
+  styleUrls: ['./new-user.component.scss'],
 })
-
 export class NewUserComponent implements OnInit {
+  user: User = new User();
+  rptPassword: string = '';
 
-  user: User = new User()
-  rptPassword: string = ''
+  constructor(private router: Router, private snackBar: MatSnackBar) {}
+  passwordValidator: ValidatorFn = (
+    control: FormGroup
+  ): ValidationErrors | null => {
+    const password = control.get('password').value;
+    const rptPassword = control.get('rptPassword').value;
+    return password === rptPassword ? null : { passwordInvalid: true };
+  };
 
-  constructor(private router: Router, private snackBar: MatSnackBar) { }
+  newUserForm = new FormGroup(
+    {
+      name: new FormControl('', [Validators.required]),
+      cpf: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required]),
+      rptPassword: new FormControl('', [Validators.required]),
+    },
+    { validators: this.passwordValidator }
+  );
 
-  newUserForm = new FormGroup({
-    name: new FormControl('', [Validators.required]),
-    cpf: new FormControl('', [Validators.required]),
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required]),
-    rptPassword: new FormControl('', [Validators.required]),
-  });
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   return(): void {
-
-    this.router.navigate(['app', 'user'])
+    this.router.navigate(['app', 'user']);
   }
 
   registerUser() {
-
-    if (this.user.password != this.rptPassword) {
-
-      this.snackBar.open('ERRO: Senhas não coincidem', 'Retornar', {
-        panelClass: "snackbar",
-        duration: 6000,
-        horizontalPosition: "center",
-        verticalPosition: "bottom"
-      })
-
-      return
-    }
-
     this.snackBar.open('Usuário cadastrado com sucesso', 'Ok', {
-      panelClass: "snackbar",
+      panelClass: 'snackbar',
       duration: 6000,
-      horizontalPosition: "center",
-      verticalPosition: "bottom"
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom',
     });
 
     this.return();
